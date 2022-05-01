e0210af1e21cec6b50e58909654f25a629880760
oluschi <72060680+oluschi@users.noreply.github.com>
2022-04-30 23:08:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// About Page PopUp //
+
+function aboutPopUp() {
+	var about = document.getElementById("aboutPop");
+	about.classList.toggle("show");
+	//popupAbout.classList.toggle("show");//
+	
+	// About Draggle Function//
+
+	$(document).ready(function() {
+		$("#dragAbout").draggable();
+		$("#dragAbout").keydown(function() {
+		});
+		$("#dragAbout").keyup(function() {	
+		});
+	});
+};
+
+
+		// Share Draggle Function //
+
+$(function () {
+		// body...
+	}).ready(function() {
+		$(".container").draggable();
+		$(".container").keydown(function() {
+		});
+		$(".container").keyup(function() {	
+		});
+	});
+;
+
+
+
+
+
